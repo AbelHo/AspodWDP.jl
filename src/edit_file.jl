2c78a6180150e5c7d0ad271e57322c51545da6cf
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-05-31 19:49:27 -1000

    Update edit_file.jl

--- src/edit_file.jl
+++ src/edit_file.jl
@@ -1,5 +1,5 @@
 function analyse_auto()
     open(joinpath(homedir(), "Desktop/aspod/test.command"), "w") do file
-        write(file, "this is a test")
+        write(file, "#!/usr/bin/env zsh\n echo edited this text file 2022-06-01T01:50")
     end
 end
