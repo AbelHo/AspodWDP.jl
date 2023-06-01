65e2266bd07f8f63c13275d2e361302125ec1ee7
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-06-01 01:29:53 -0400

    feat: edit file

--- src/AspodWDP.jl
+++ src/AspodWDP.jl
@@ -1,9 +1,9 @@
 module AspodWDP
 #export cal_imgsfol, cal_imgsVideo, calibrate_video_checkerboard
 # Write your package code here.
-#include("init.jl")
+include("edit_file.jl")
 #include("projection.jl")
 greet() = print("Hello World!")
-analyse_auto() = println("Analysis temporary interface begins....\n\n\n\n")
+# analyse_auto() = println("Analysis temporary interface begins....\n\n\n\n")
 export greet, analyse_auto
 end
---
+++ src/edit_file.jl
@@ -0,0 +1,5 @@
+function analyse_auto()
+    open("~/Desktop/aspod/test.command", "w") do file
+        write(file, "this is a test")
+    end
+end
