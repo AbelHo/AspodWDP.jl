c7e90bbcece7128435790ffe1f95c3cb6caf2bcd
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-05-31 19:47:32 -1000

    fix: homedir()

--- src/edit_file.jl
+++ src/edit_file.jl
@@ -1,5 +1,5 @@
 function analyse_auto()
-    open("~/Desktop/aspod/test.command", "w") do file
+    open(joinpath(homedir(), "Desktop/aspod/test.command"), "w") do file
         write(file, "this is a test")
     end
 end
