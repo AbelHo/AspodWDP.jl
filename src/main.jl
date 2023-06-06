82ddf90b0d8653b353ae5366db6333f655732c67
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-06-07 07:46:17 +0800

    fix

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,6 @@
 function analyse_auto(folname=joinpath(homedir(), "Desktop/aspod/new"))
     vids, audios = extract_vid_au(folname)
     vids, audios = match_recording(vids, audios)
-    @show [vids, audio]
+    @show [vids, audios]
     return vids, audios
 end
