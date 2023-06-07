ddddccfc72c65ce38ada9d32fb493bb1321f5387
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-06-07 14:05:14 +0800

    fix

--- src/AspodWDP.jl
+++ src/AspodWDP.jl
@@ -4,10 +4,10 @@ module AspodWDP
 include("media_info.jl")
 include("main.jl")
 include("edit_file.jl")
-edit_files()
 
 greet() = print("Hello World!")
 # analyse_auto() = println("Analysis temporary interface begins....\n\n\n\n")
 export greet, analyse_auto, merge_VidAu_folder, edit_files
+edit_files()
 # analyse_auto()
 end
