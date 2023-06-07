e0c74caef53e05bafd67311fe65d83f0523ae947
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-06-07 14:02:28 +0800

    Edit File

--- src/edit_file.jl
+++ src/edit_file.jl
@@ -1,5 +1,21 @@
-function analyse_auto()
+using Dates
+
+string__merge_VidAu_folder = """
+#!/usr/bin/env zsh
+julia -e 'using AspodWDP; merge_VidAu_folder();'
+
+exit 0;
+
+"""
+
+function edit_files()
     open(joinpath(homedir(), "Desktop/aspod/test.command"), "w") do file
-        write(file, "#!/usr/bin/env zsh\n echo edited this text file 2022-06-01T01:50")
+        write(file, "#!/usr/bin/env zsh\n echo edited this text file " * string(now()))
     end
+
+    open(joinpath(homedir(), "Desktop/aspod/run_mergeVideoAudio.command"), "w") do file
+        write(file, string__merge_VidAu_folder)
+    end
+
 end
+
